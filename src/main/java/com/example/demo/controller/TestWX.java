9626b681394d70a8a7d681e6d4a08de97fe3137d
dream-yl <y15617832127@163.com>
2020-07-21 10:07:25 +0800

    加入redis接口

--- src/main/java/com/example/demo/controller/TestWX.java
+++ src/main/java/com/example/demo/controller/TestWX.java
@@ -2,11 +2,11 @@ package com.example.demo.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.example.demo.entity.AccessToken;
 import com.example.demo.entity.TemplateData;
-import com.example.demo.utils.WX_HttpsUtil;
-import com.example.demo.utils.WX_TemplateMsgUtil;
-import com.example.demo.utils.WX_TokenUtil;
-import com.example.demo.utils.WX_UserUtil;
+import com.example.demo.utils.*;
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,6 +18,8 @@ import java.util.Map;
 @Controller
 @RequestMapping("/weChat")
 public class TestWX {
+    @Autowired
+    private RedisConfig RedisConfig;
     /*public static void main(String[] args) {
         //新增用户成功 - 推送微信消息
         senMsg("oTQdNwz-gUeDfLgAQSiRhLZzj1O4");
@@ -28,6 +30,22 @@ public class TestWX {
         senMsg("oTQdNwz-gUeDfLgAQSiRhLZzj1O4");
         return "success";
     }
+
+    /**
+     * 测试redis
+     * @return
+     */
+    @RequestMapping("/redis")
+    @ResponseBody
+    public String ceshi(){
+        AccessToken accessToken =RedisConfig.getAccessToken();
+        if (accessToken == null){
+            RedisConfig.setAccessToken(WX_TokenUtil.getWXToken().getAccessToken(),10);
+            return "新增缓存"+RedisConfig.getAccessToken().getAccessToken();
+        }
+        return "老数据"+accessToken.getAccessToken();
+    }
+
     static void senMsg(String openId){
         //用户是否订阅该公众号标识 (0代表此用户没有关注该公众号 1表示关注了该公众号)
         Integer  state= WX_UserUtil.subscribeState(openId);
