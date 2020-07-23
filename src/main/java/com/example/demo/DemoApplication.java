801867f9bfba6f2295346fddb679f165233dbf44
dream-yl <y15617832127@163.com>
2020-07-23 16:32:09 +0800

    修改了注释

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 启动类注解
+ * 启动类注解  
  */
 @SpringBootApplication
 public class DemoApplication {
